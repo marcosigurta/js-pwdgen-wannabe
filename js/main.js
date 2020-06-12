b5ae15a3cf0191e18acc0f167b7d21762bd8be3f
marco <marcosigurta.ovb@gmail.com>
2020-06-12 13:54:16 +0200

    Form Imput JS

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Form = Insert User Name
+
+var name = prompt("Insert your Name");
+
+//Form = insert user surname
+
+var surname = prompt("Insert your Surname");
+
+//Form = favourite Color
+
+var favColor = prompt("Almost done! Insert your Favorite Colour");
+
+//var Number
+
+var number = 19;
+
+//Input Results
+var personalData = (document.getElementById("personal-data").style.display =
+    "block");
+var personalData = (document.getElementById("personal-data").innerHTML =
+    name + surname + favColor + number);
